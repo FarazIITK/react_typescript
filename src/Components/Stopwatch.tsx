bc674ff14894179ab70b8974a6f91146727f24eb
fhusain01 <fhusain@adaptready.com>
2023-04-16 17:01:57 +0530

    Added stopwatch component with basix logic

---
+++ src/Components/Stopwatch.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef, useEffect } from 'react';
+
+const Stopwatch = () => {
+  const [timeElapsed, setTimeElapsed] = useState<number>(0);
+  const [isTimerPaused, setIsTimerPaused] =
+    useState<boolean>(true);
+
+  const intervalId = useRef<NodeJS.Timer | null>(null);
+
+  const playPauseHandler = () => {
+    setIsTimerPaused((prevState) => !prevState);
+  };
+
+  useEffect(() => {
+    if (isTimerPaused && intervalId.current) {
+      clearInterval(intervalId.current);
+    } else if (!isTimerPaused) {
+      intervalId.current = setInterval(() => {
+        setTimeElapsed((prevValue) => prevValue + 1);
+      }, 1000);
+    }
+  }, [isTimerPaused]);
+
+  return (
+    <div>
+      <h1>Time: {timeElapsed}</h1>
+      <div>
+        <button onClick={playPauseHandler}>Play</button>
+        <button onClick={playPauseHandler}>Pause</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
